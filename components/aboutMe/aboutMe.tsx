deac576b31dd1d63523415d0a375fb5075f7a20c
Abderaouf16 <abderaoufa88@gmail.comm>
2026-06-05 23:09:24 +0100

    Edit: edited about section text + updated cv file

--- components/aboutMe/aboutMe.tsx
+++ components/aboutMe/aboutMe.tsx
@@ -8,7 +8,7 @@ import { annotate, annotationGroup } from "rough-notation";
 import feather from "feather-icons";
 
 function AboutMe() {
-  const cv = "/assets/CV/cv.pdf";
+  const cv = "/assets/CV/cv new.pdf";
 
   const [animationCompleted, setAnimationCompleted] = useState(false);
 
@@ -26,7 +26,6 @@ function AboutMe() {
         const el7 = document.querySelector("#e7") as HTMLElement | null;
         const el8 = document.querySelector("#e8") as HTMLElement | null;
 
-
         if (el1 && el2 && el3 && el4 && el5 && el6 && el7 && el8) {
           const a1 = annotate(el1, { type: "highlight", color: "#93c5fd" });
           const a2 = annotate(el2, { type: "highlight", color: "#93c5fd" });
@@ -35,10 +34,9 @@ function AboutMe() {
           const a5 = annotate(el5, { type: "highlight", color: "#93c5fd" });
           const a6 = annotate(el6, { type: "highlight", color: "#93c5fd" });
           const a7 = annotate(el7, { type: "circle", color: "#d4c8b9" });
-          const a8 = annotate(el8, { type: "highlight", color: "#e0c297"});
+          const a8 = annotate(el8, { type: "highlight", color: "#e0c297" });
 
-
-          const ag = annotationGroup([a8, a1, a2, a3, a4, a5, a6, a7 ]);
+          const ag = annotationGroup([a8, a1, a2, a3, a4, a5, a6, a7]);
           ag.show();
         }
       }, delay); // Delay in milliseconds
@@ -67,12 +65,13 @@ function AboutMe() {
             <div className="flex-1 md:mr-20 ">
               <h6 className="  font-bold text-gray-600 text-2xl  lg:text-3xl ">
                 Hello ! I’m Abderraouf, a{" "}
-                <span id="e8" className="inline-block" >Designer</span>
-                <span > / </span>
+                <span id="e8" className="inline-block">
+                  Designer
+                </span>
+                <span> / </span>
                 <span id="e1" className="inline-block">
                   full stack developer
                 </span>{" "}
-                bassed in Algeria
               </h6>
               <h1 className="font-normal text-gray-900 text-4xl md:text-7xl leading-none mb-8"></h1>
               <div className="font-normal text-gray-600 text-sm  lg:text-base mb-16">
@@ -86,11 +85,11 @@ function AboutMe() {
                 </p>
                 <p className="mb-2">
                   {" "}
-                  My journey of coding started when entering{" "}
+                  My journey in the digital started when entering{" "}
                   <span id="e4">architecture</span> school and working with it
                   softwares made me wonder how they design & built it, This
-                  curiosity pushed me to start a career as a full-stask
-                  developer.
+                  curiosity pushed me to start a career as a Designer /
+                  full-stask developer.
                 </p>
                 <p className="mb-2">
                   I was a student at Code labs academy School where I spent 10
@@ -98,7 +97,7 @@ function AboutMe() {
                   <span id="e5" className="inline-block">
                     front-end and back-end
                   </span>{" "}
-                  web development and the{" "}
+                  web development and Design principles combined with{" "}
                   <span id="e6" className="inline-block">
                     {" "}
                     flexible skills
@@ -106,9 +105,12 @@ function AboutMe() {
                   to succeed in this field
                 </p>
                 <p className="mb-2">
-                  I'm currently looking for a new role as a developer.{" "}
                   <span className="inline-block" id="e7">
-                    Hire me?
+                    Hire me
+                  </span>
+                  <span className="px-1">
+                    {" "}
+                    as your next designer / full stack developer ?
                   </span>
                 </p>
               </div>
